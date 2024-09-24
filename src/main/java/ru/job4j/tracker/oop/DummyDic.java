9ff0b93b02fa4d1c956481f91ff4a3cf1ff526b0
Denis Mukhin <denis.m.mukhin@gmail.com>
2024-09-24 20:22:09 +0300

    1.4. Вызов метода с возвращаемым типом. [#167387]

--- src/main/java/ru/job4j/tracker/oop/DummyDic.java
+++ src/main/java/ru/job4j/tracker/oop/DummyDic.java
@@ -5,4 +5,10 @@ public class DummyDic {
         String translate = "Неизвестное слово " + eng;
         return translate;
     }
+
+    public static void main(String[] args) {
+        DummyDic dummyDic = new DummyDic();
+        String translate = dummyDic.engToRus("car");
+        System.out.println(translate);
+    }
 }
