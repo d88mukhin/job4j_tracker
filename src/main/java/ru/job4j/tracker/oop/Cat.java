604cb881d69373ca7956471c4f7726904e2ff19b
Denis Mukhin <denis.m.mukhin@gmail.com>
2024-09-25 07:04:45 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/tracker/oop/Cat.java
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -1,14 +1,32 @@
 package ru.job4j.tracker.oop;
 
 public class Cat {
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    private String name;
+    private String food;
+
+    public void show() {
+        System.out.println("Name : " + this.name);
+        System.out.println("Food : " + this.food);
+    }
+
+    public void giveName(String nik) {
+        this.name = nik;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There is gav's food");
+        Cat gav = new Cat();
+        gav.giveName("gav");
+        gav.eat("cutlet");
+        gav.show();
+        System.out.println("There is black,s food");
+        Cat black = new Cat();
+        black.giveName("black");
+        black.eat("fish");
+        black.show();
     }
 }
